a1acbea070b2526c585a95b0b126eea0b5d80784
Yirogu <stachudcs@gmail.com>
2019-03-22 18:32:41 +0100

    Add reset button , now the language is changing in the console

--- src/lib/MainGui.py
+++ src/lib/MainGui.py
@@ -3,31 +3,32 @@ from tkinter import messagebox
 from .Translation import Translation
 # how i can do 1 import for 2 files ?
 class MainGui():
-    CurrentLangue = "Pl"
+    CurrentLangue = 'Pl'
     def __init__(self, master):
 
         self.master = master
         master.title("Golf Time Pro")
         variable = StringVar(master)
-        variable.set("Pl")
+        variable.set('Eng')
 
-
-
-
-        self.popupMenu = OptionMenu(master, variable,"Eng","Pl")
+        self.popupMenu = OptionMenu(master, variable,'Eng','Pl' )
         self.popupMenu.pack()
-        if variable.get() != self.CurrentLangue :
-            self.CurrentLangue =variable.get()
-            messagebox.showinfo("Alert", "Restart application for change langue")
+
 
         self.label = Label(master, text=Translation.translator("Welcome on our application",self.CurrentLangue))
         self.label.pack()
 
         self.greet_button = Button(master, text=Translation.translator("Greet",self.CurrentLangue), command=self.greet)
         self.greet_button.pack()
+        self.reset_button = Button(master, text=Translation.translator("reset",self.CurrentLangue), command=lambda:MainGui.reset(variable.get(),master))
+        self.reset_button.pack()
 
         self.close_button = Button(master, text=Translation.translator("Close",self.CurrentLangue), command=master.quit)
         self.close_button.pack()
 
+    def reset(variable,master):
+        if variable != MainGui.CurrentLangue :
+            MainGui.CurrentLangue = variable
+            print(MainGui.CurrentLangue)
     def greet(self):
         print(Translation.translator("Welcome on our application",self.CurrentLangue))
