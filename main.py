6d2ce71f09102f20e9b63a58715e1096225fae97
Yirogu <stachudcs@gmail.com>
2019-03-22 17:37:25 +0100

    Trying reload menu langue, add info about restart application for change langue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.lib.MainGui import MainGui
-from tkinter import Tk, Label, Button
+from tkinter import *
 root = Tk()
 my_gui = MainGui(root)
 root.mainloop()
--- src/lib/MainGui.py
+++ src/lib/MainGui.py
@@ -1,21 +1,33 @@
-from tkinter import Tk, Label, Button
+from tkinter import *
+from tkinter import messagebox
 from .Translation import Translation
 # how i can do 1 import for 2 files ?
 class MainGui():
-
+    CurrentLangue = "Pl"
     def __init__(self, master):
-        CurrentLangue = "Pl"
+
         self.master = master
         master.title("Golf Time Pro")
+        variable = StringVar(master)
+        variable.set("Pl")
 
-        self.label = Label(master, text=Translation.translator("Welcome on our application",CurrentLangue))
+
+
+
+        self.popupMenu = OptionMenu(master, variable,"Eng","Pl")
+        self.popupMenu.pack()
+        if variable.get() != self.CurrentLangue :
+            self.CurrentLangue =variable.get()
+            messagebox.showinfo("Alert", "Restart application for change langue")
+
+        self.label = Label(master, text=Translation.translator("Welcome on our application",self.CurrentLangue))
         self.label.pack()
 
-        self.greet_button = Button(master, text=Translation.translator("Greet",CurrentLangue), command=self.greet)
+        self.greet_button = Button(master, text=Translation.translator("Greet",self.CurrentLangue), command=self.greet)
         self.greet_button.pack()
 
-        self.close_button = Button(master, text=Translation.translator("Close",CurrentLangue), command=master.quit)
+        self.close_button = Button(master, text=Translation.translator("Close",self.CurrentLangue), command=master.quit)
         self.close_button.pack()
 
     def greet(self):
-        print(Translation.translator("Welcome on our application",CurrentLangue))
+        print(Translation.translator("Welcome on our application",self.CurrentLangue))
