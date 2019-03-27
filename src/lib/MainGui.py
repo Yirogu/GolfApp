d963b23ed54b15fd175dfbeed7db961dadf80cb0
Yirogu <stachudcs@gmail.com>
2019-03-27 23:19:09 +0100

    Add Translation method to all words and add some comments ;)

--- src/lib/MainGui.py
+++ src/lib/MainGui.py
@@ -10,7 +10,6 @@ class MainGui(tk.Tk):
     def __init__ (self,*args,**kwargs) :
 
         tk.Tk.__init__(self,*args,**kwargs)
-
         tk.Tk.title(self,"Golf Time Pro")
         img = tk.PhotoImage(file='/home/stach/Desktop/Python3/GolfApp/src/lib/logo.gif')
         self.tk.call('wm','iconphoto',self._w,img)
@@ -21,7 +20,7 @@ class MainGui(tk.Tk):
         container.grid_columnconfigure(0,weight = 1)
 
         self.frames = {}
-
+        # Place t o add next Frames
         for F in (StartPage,About_Page,Settings_Page) :
 
             frame  = F(container,self)
@@ -45,11 +44,11 @@ class StartPage (tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
 
-        button1 = ttk.Button(self,text = "About",
+        button1 = ttk.Button(self,text = Translation.translator("About us",MainGui.CurrentLangue),
         command = lambda :controller.show_frame(About_Page))
         button1.pack()
 
-        button2 = ttk.Button(self,text = "Settings",
+        button2 = ttk.Button(self,text = Translation.translator("Settings",MainGui.CurrentLangue),
         command = lambda :controller.show_frame(Settings_Page))
         button2.pack()
 
@@ -63,34 +62,35 @@ class StartPage (tk.Frame):
 class About_Page(tk.Frame) :
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self,text="About us",font =LARGE_FONT)
+        label = tk.Label(self,text=Translation.translator("About us",MainGui.CurrentLangue),font =LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = ttk.Button(self,text = "Back to Home",
+        button1 = ttk.Button(self,text = Translation.translator("Back to Home",MainGui.CurrentLangue),
         command = lambda :controller.show_frame(StartPage))
         button1.pack()
 
 class Settings_Page(tk.Frame) :
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self,text="Settings",font =LARGE_FONT)
+        label = tk.Label(self,text=Translation.translator("Settings",MainGui.CurrentLangue),font =LARGE_FONT)
         label.pack(pady=10,padx=10)
         variable = tk.StringVar(parent)
         variable.set(Settings.CurrentLangue())
         leng = ["Eng","Pl"]
-        label = tk.Label(self,text="Langue",font =LARGE_FONT)
+        label = tk.Label(self,text=Translation.translator("Langue",MainGui.CurrentLangue),font =LARGE_FONT)
         label.pack(pady=10,padx=10)
         self.popupMenu = tk.OptionMenu(self, variable,*leng)
         self.popupMenu.pack()
 
-        button1 = ttk.Button(self,text = "Save",
+        button1 = ttk.Button(self,text = Translation.translator("Save",MainGui.CurrentLangue),
         command = lambda :Settings_Page.reset(variable.get()))
         button1.pack()
 
-        button2 = ttk.Button(self,text = "Back to Home",
+        button2 = ttk.Button(self,text = Translation.translator("Back to Home",MainGui.CurrentLangue),
         command = lambda :controller.show_frame(StartPage))
         button2.pack()
     def reset(variable):
+        # Check current langue if is diffrent, expect change after reset
         if variable != MainGui.CurrentLangue :
             Settings.saveLangue(MainGui.CurrentLangue,variable)
             print(MainGui.CurrentLangue)
--- src/lib/Translation.py
+++ src/lib/Translation.py
@@ -1,27 +1,33 @@
 class Translation():
+
     """set polish or english langue"""
-    words = {'Close' : {'Eng':'Close','Pl':'Zamknij'},
-            'Greet' : {'Eng':'Greet','Pl':'Powitanie'},
-            'Start' : {'Eng':'Start','Pl':'Rozpocznij'}
 
+    words = {'Close' : {'Eng' : 'Close','Pl' : 'Zamknij'},
+            'Greet' : {'Eng' : 'Greet','Pl' : 'Powitanie'},
+            'Langue' : {'Eng' : 'Langue','Pl' : 'Jezyk'},
+            'Save' : {'Eng':'Save','Pl' : 'Zapisz'},
+            'Settings' : {'Eng' : 'Settings','Pl' : 'Ustawienia'},
+            'Start' : {'Eng' : 'Start','Pl' : 'Rozpocznij'}}
 
-        }
-    sentences = {'Welcome on our application' :
-        {'Eng':'Welcome on our application','Pl':'Witamy w naszej aplikacji'}
-
-            }
+    sentences = {'About us' :
+        {'Eng' : 'About us','Pl' : 'O Nas'},
+    'Back to Home' :
+        {'Eng' : 'Back to Home','Pl' : 'Wroc do menu'},
+    'Welcome on our application' :
+        {'Eng' : 'Welcome on our application','Pl' : 'Witamy w naszej aplikacji'}}
 
     def translator(text,langue = 'Eng') :
         # function is trying to translate a sentence or word
         # if it can not do it, return text
+
         if len(text.split(" ")) > 1 :
             return Translation.sentenceTranslate(text,langue)
         else :
             return Translation.wordsTranslate(text,langue)
 
     def sentenceTranslate(text,langue):
-# This function is part of the translator (),
- # and it supports sentences
+        # This function is part of the translator (),
+        # and it supports sentences
 
         sentences = Translation.sentences
         if text in sentences :
@@ -31,8 +37,8 @@ class Translation():
             return text
 
     def wordsTranslate(text,langue):
-# This function is part of the translator (),
-# and it supports words
+        # This function is part of the translator (),
+        # and it supports words
 
         words = Translation.words
         if text in words :
