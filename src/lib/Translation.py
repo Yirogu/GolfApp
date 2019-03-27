6c4f9025b50fbf4d096a51516a24b42183685b35
Yirogu <stachudcs@gmail.com>
2019-03-27 23:20:21 +0100

    Add Translation method to all words and add some comments ;)

--- src/lib/Translation.py
+++ src/lib/Translation.py
@@ -2,19 +2,21 @@ class Translation():
 
     """set polish or english langue"""
 
-    words = {'Close' : {'Eng' : 'Close','Pl' : 'Zamknij'},
+    words = {
+            'Close' : {'Eng' : 'Close','Pl' : 'Zamknij'},
             'Greet' : {'Eng' : 'Greet','Pl' : 'Powitanie'},
             'Langue' : {'Eng' : 'Langue','Pl' : 'Jezyk'},
             'Save' : {'Eng':'Save','Pl' : 'Zapisz'},
             'Settings' : {'Eng' : 'Settings','Pl' : 'Ustawienia'},
             'Start' : {'Eng' : 'Start','Pl' : 'Rozpocznij'}}
 
-    sentences = {'About us' :
-        {'Eng' : 'About us','Pl' : 'O Nas'},
-    'Back to Home' :
-        {'Eng' : 'Back to Home','Pl' : 'Wroc do menu'},
-    'Welcome on our application' :
-        {'Eng' : 'Welcome on our application','Pl' : 'Witamy w naszej aplikacji'}}
+    sentences = {
+            'About us' :
+                {'Eng' : 'About us','Pl' : 'O Nas'},
+            'Back to Home' :
+                {'Eng' : 'Back to Home','Pl' : 'Wroc do menu'},
+            'Welcome on our application' :
+                {'Eng' : 'Welcome on our application','Pl' : 'Witamy w naszej aplikacji'}}
 
     def translator(text,langue = 'Eng') :
         # function is trying to translate a sentence or word
