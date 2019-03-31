b12c234f3da5a9c861a3bb19f74cfecbe088b791
Yirogu <stachudcs@gmail.com>
2019-03-31 14:04:48 +0200

    Add Refresh class

--- src/lib/MainGui.py
+++ src/lib/MainGui.py
@@ -7,8 +7,9 @@ from .Refresh import Refresh
 
 # how i can do 1 import for 2 files ?
 LARGE_FONT = ("Verdana", 12)
+CurrentLangue = Settings.CurrentLangue()
 class MainGui(tk.Tk):
-    CurrentLangue = Settings.CurrentLangue()
+
     def __init__ (self,*args,**kwargs) :
 
         tk.Tk.__init__(self,*args,**kwargs)
@@ -46,28 +47,28 @@ class StartPage (tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
 
-        button1 = ttk.Button(self,text = Translation.translator("About us",MainGui.CurrentLangue),
+        button1 = ttk.Button(self,text = Translation.translator("About us",CurrentLangue),
         command = lambda :controller.show_frame(About_Page))
         button1.pack()
 
-        button2 = ttk.Button(self,text = Translation.translator("Settings",MainGui.CurrentLangue),
+        button2 = ttk.Button(self,text = Translation.translator("Settings",CurrentLangue),
         command = lambda :controller.show_frame(Settings_Page))
         button2.pack()
 
-        self.close_button = ttk.Button(self, text=Translation.translator("Close",MainGui.CurrentLangue), command=self.quit)
+        self.close_button = ttk.Button(self, text=Translation.translator("Close",CurrentLangue), command=self.quit)
         self.close_button.pack()
 
 
 
     def greet(self):
-        print(Translation.translator("Welcome on our application",MainGui.CurrentLangue))
+        print(Translation.translator("Welcome on our application",CurrentLangue))
 class About_Page(tk.Frame) :
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self,text=Translation.translator("About us",MainGui.CurrentLangue),font =LARGE_FONT)
+        label = tk.Label(self,text=Translation.translator("About us",CurrentLangue),font =LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = ttk.Button(self,text = Translation.translator("Back to Home",MainGui.CurrentLangue),
+        button1 = ttk.Button(self,text = Translation.translator("Back to Home",CurrentLangue),
         command = lambda :controller.show_frame(StartPage))
         button1.pack()
 
@@ -75,22 +76,22 @@ class About_Page(tk.Frame) :
 class Settings_Page(tk.Frame) :
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self,text=Translation.translator("Settings",MainGui.CurrentLangue),font =LARGE_FONT)
+        label = tk.Label(self,text=Translation.translator("Settings",CurrentLangue),font =LARGE_FONT)
         label.pack(pady=10,padx=10)
         variable = tk.StringVar(parent)
         variable.set(Settings.CurrentLangue())
         leng = ["Eng","Pl"]
-        label = tk.Label(self,text=Translation.translator("Langue",MainGui.CurrentLangue),font =LARGE_FONT)
+        label = tk.Label(self,text=Translation.translator("Langue",CurrentLangue),font =LARGE_FONT)
         label.pack(pady=10,padx=10)
         self.popupMenu = tk.OptionMenu(self, variable,*leng)
         self.popupMenu.pack()
 
 
-        button1 = ttk.Button(self,text = Translation.translator("Save",MainGui.CurrentLangue),
+        button1 = ttk.Button(self,text = Translation.translator("Save",CurrentLangue),
         command = lambda : Refresh.refresh(variable.get(),label))
         button1.pack()
 
 
-        button2 = ttk.Button(self,text = Translation.translator("Back to Home",MainGui.CurrentLangue),
+        button2 = ttk.Button(self,text = Translation.translator("Back to Home",CurrentLangue),
         command = lambda :controller.show_frame(StartPage))
         button2.pack()
--- src/lib/Refresh.py
+++ src/lib/Refresh.py
@@ -3,7 +3,7 @@ from .Translation import Translation
 class Refresh :
     def refresh(variable,element):
         # Check current langue if is diffrent, expect change after reset
-        if variable != MainGui.CurrentLangue :
-            Settings.saveLangue(MainGui.CurrentLangue,variable)
-            MainGui.CurrentLangue = variable
-            return element.config(text = Translation.translator("Langue",MainGui.CurrentLangue))
+        if variable != Settings.CurrentLangue() :
+            Settings.saveLangue(Settings.CurrentLangue(),variable)
+            CurrentLangue = variable
+            return element.config(text = Translation.translator("Langue",CurrentLangue)),CurrentLangue
